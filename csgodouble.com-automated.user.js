8f37d491f0382480434bfe8e0a3677bcfdd37727
Mole <aareksio@gmail.com>
2016-01-05 16:30:30 +0100

    Fixed error while pausing/aborting, version bump

--- csgodouble.com-automated.user.js
+++ csgodouble.com-automated.user.js
@@ -2,7 +2,7 @@
 // @name            csgodouble.com - automated
 // @description     An userscript that automates csgodouble.com betting using martingale system.
 // @namespace       automated@mole
-// @version         1.01
+// @version         1.02
 // @author          Mole
 // @match           http://www.csgodouble.com/*
 // @run-at          document-end
@@ -306,11 +306,13 @@ Automated.prototype.start = function() {
 };
 
 Automated.prototype.stop = function() {
+    clearInterval(this.game);
     this.game = null;
     this.running = false;
 };
 
 Automated.prototype.abort = function() {
+    clearInterval(this.game);
     this.game = null;
     this.running = false;
     this.last_result = 'abort';
