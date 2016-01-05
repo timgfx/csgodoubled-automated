03539542f0b0af9b230fc94721bbf1a256eef19d
Mole <aareksio@gmail.com>
2016-01-06 00:33:36 +0100

    Version bump

--- csgodouble.com-automated.user.js
+++ csgodouble.com-automated.user.js
@@ -2,7 +2,7 @@
 // @name            csgodouble.com - automated
 // @description     An userscript that automates csgodouble.com betting using martingale system.
 // @namespace       automated@mole
-// @version         1.16
+// @version         1.17
 // @author          Mole
 // @match           http://www.csgodouble.com/*
 // @run-at          document-end
